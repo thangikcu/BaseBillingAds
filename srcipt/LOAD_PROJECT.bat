1992512c63b64c743c389d85c4d70302f5b140ae
ThangTV18 <thanggun99@gmail.com>
2022-07-30 22:40:30 +0700

    update script

--- srcipt/LOAD_PROJECT.bat
+++ srcipt/LOAD_PROJECT.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
-CALL git clone -b dev https://gitlab.com/mmg-app/easycounting.git
+CALL git clone -b dev https://gitlab.com/mmg-app/qrbarcodeandandroidgenerator.git
 
 @REM Sửa tên này = XXX trong URL https://gitlab.com/.../[XXX].git
-CD easycounting
+CD qrbarcodeandandroidgenerator
 
+CALL git checkout -b amazon
 CALL git remote add BuildJob https://gitlab.com/cicd210/building-artifacts.git
 EXIT
